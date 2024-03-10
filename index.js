49f82eb65c917ec8a66d68a47609e219c4d620a2
jay323p <jay.ashok.p@gmail.com>
2024-03-10 19:54:12 -0400

    res.header middleware fix

--- index.js
+++ index.js
@@ -23,28 +23,28 @@ app.use(
   })
 );
 
-app.use(function (req, res, next) {
-  //   res.header(
-  //     'Access-Control-Allow-Origin',
-  //     'https://resonant-cranachan-6ad07d.netlify.app'
-  //   );
-  //   res.header(
-  //     'Access-Control-Allow-Headers',
-  //     'Origin, X-Requested-With, Content-Type, Accept'
-  //   );
-  //   next();
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://resonant-cranachan-6ad07d.netlify.app'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'POST, GET, PUT, PATCH, DELETE'
-  );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+// app.use(function (req, res, next) {
+//   //   res.header(
+//   //     'Access-Control-Allow-Origin',
+//   //     'https://resonant-cranachan-6ad07d.netlify.app'
+//   //   );
+//   //   res.header(
+//   //     'Access-Control-Allow-Headers',
+//   //     'Origin, X-Requested-With, Content-Type, Accept'
+//   //   );
+//   //   next();
+//   res.setHeader(
+//     'Access-Control-Allow-Origin',
+//     'https://resonant-cranachan-6ad07d.netlify.app'
+//   );
+//   res.setHeader(
+//     'Access-Control-Allow-Methods',
+//     'POST, GET, PUT, PATCH, DELETE'
+//   );
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  next();
-});
+//   next();
+// });
 
 app.get('/', (req, res) => {
   res.send('Welcome');
